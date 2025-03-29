6ff17212eb46b497e1ea098a7f6adb26aa6d8cda
Ranjit.KumarPandit <Ranjit.KumarPandit@contractor.tatacommunications.com>
2025-03-29 14:08:23 +0530

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    logger: ['debug', 'error', 'log', 'verbose','warn'],
+    logger: ['debug', 'error', 'log', 'verbose', 'warn'],
   });
   app.setGlobalPrefix('api');
   app.enableVersioning({
     type: VersioningType.URI,
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Tickete App Endpoints')
+    .setVersion('1.0')
+    .addTag('experience')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
